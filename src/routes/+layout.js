2a68ac43a8362da6d37bbcab59e273bc184116a9
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-06-15 00:07:34 +0100

    revert to gh-pages as cdn

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,7 +3,7 @@ import { locs_path, config_path, sheets_path } from "$lib/config";
 import { getDataUrl, getPlaces, getConfig, getSheets } from "$lib/utils";
 
 export async function load({ fetch }) {
-	let data_url = await getDataUrl(false, fetch);
+	let data_url = await getDataUrl(true, fetch);
 	let places = await getPlaces(`${data_url}/${locs_path}`, fetch);
 	let config = await getConfig(`${data_url}/${config_path}`, fetch);
 	let sheets = await getSheets(`${data_url}/${sheets_path}`, config, fetch);
