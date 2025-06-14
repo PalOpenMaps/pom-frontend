366b37765ff491fe79cebca121490fc9c8a57932
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-06-14 23:30:20 +0100

    test version for cf pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,35 +1,45 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
-import adapter_node from '@sveltejs/adapter-node';
+// import adapter_static from '@sveltejs/adapter-static';
+// import adapter_node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 
-const production = process.env.NODE_ENV === 'production';
-const node = process.env.APP_ENV === 'node';
+// const production = process.env.NODE_ENV === 'production';
+// const node = process.env.APP_ENV === 'node';
 
-const config = node ? 
-{
+const config = {
 	kit: {
-    adapter: adapter_node(),
+    adapter: adapter(),
     paths: {
       base: ''
 		}
 	}
-} :
-{
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: adapter({
-			// Options below are defaults
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html'
-		}),
-		prerender: {
-			handleHttpError: 'warn'
-		},
-		paths: {
-			base: production ? '/pom-frontend' : ''
-		}
-	}
 };
+
+// const config = node ? 
+// {
+// 	kit: {
+//     adapter: adapter_node(),
+//     paths: {
+//       base: ''
+// 		}
+// 	}
+// } :
+// {
+// 	kit: {
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		adapter: adapter_static({
+// 			// Options below are defaults
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: '404.html'
+// 		}),
+// 		prerender: {
+// 			handleHttpError: 'warn'
+// 		},
+// 		paths: {
+// 			base: production ? '/pom-frontend' : ''
+// 		}
+// 	}
+// };
 
 export default config;
