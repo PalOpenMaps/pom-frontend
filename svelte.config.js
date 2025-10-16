3b047a11f88093ced87a383f0567813cb2d16485
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-10-16 20:21:57 +0100

    move to netlify preview

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@sveltejs/kit').Config} */
 import adapter_static from '@sveltejs/adapter-static';
 import adapter_node from '@sveltejs/adapter-node';
-import adapter_cloudflare from '@sveltejs/adapter-cloudflare';
+import adapter_netlify from '@sveltejs/adapter-netlify';
 
 const mode = process.env.APP_ENV ? process.env.APP_ENV : process.env.NODE_ENV === 'production' ? 'preview' : 'dev';
 const base = mode === 'preview' ? '/pom-frontend' : '';
@@ -9,8 +9,8 @@ const base = mode === 'preview' ? '/pom-frontend' : '';
 const config =
 	mode === 'node'
 		? { kit: { adapter: adapter_node() } }
-		: mode === 'cloudflare'
-		? { kit: { adapter: adapter_cloudflare() } }
+		: mode === 'netlify'
+		? { kit: { adapter: adapter_netlify() } }
 		: {
 				kit: {
 					adapter: adapter_static({
